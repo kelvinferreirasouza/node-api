0e417d277d46fec90eb3fe2de6b5adea68f7ea77
Kelvin Souza <kelvinferreirasouza1@gmail.com>
2020-03-27 19:01:35 -0300

    Adicionado as funções de Show, Store, Update e Destroy

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
+app.use(express.json());
 
 // Iniciando o DB
 mongoose.connect(
-    'mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+    "mongodb://localhost:27017/nodeapi", { useNewUrlParser: true }
+);
 
-requireDir('./src/models');
+requireDir("./src/models");
 
 // Rotas
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
